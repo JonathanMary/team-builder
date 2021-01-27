71dd85d8ffaa5c798f8b38ac88c9e5fc17fb2b06
JonathanMary <jon85ma@gmail.com>
2021-01-27 02:35:31 +0100

    MVP: done

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import Form from './Form.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState } from 'react';
+
+const initialFormValues= {
+  name: "",
+  email: "",
+  role: "",
 }
+
+function App(){
+
+    const [teamMembers, setTeamMembers] = useState([]);
+    const [formData, setFormData] = useState(initialFormValues);
+
+    //when form change, save it into formData
+    const onChange = evt => {
+        setFormData({
+            ...formData, 
+            [evt.target.name] : evt.target.value
+        })
+    }
+
+    //when user press submit, push form data into teamMembers.
+    const onSubmit = evt => {
+        evt.preventDefault();
+        setTeamMembers([...teamMembers, formData]);
+        //console.log("teamMembers: ", teamMembers);
+    }
+
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Form values={"formValues"} onSubmit={onSubmit} onChange={onChange}/>
+          {teamMembers.map(member => {
+            return(
+              <div className="teamMember" key={member.name}>
+                <div className="Name">{member.name.trim()}</div>
+                <div className="Email">{member.email.trim()}</div>
+                <div className="Role">{member.role}</div>
+              </div>
+            )
+          })}
+        </header>
+      </div>
+    );
+};
+
+
 
 export default App;
